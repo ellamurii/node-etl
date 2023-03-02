207a43ffd275dd739b9be13b8e97b5e7f1efcd7e
ellamurii <emb.baguio@gmail.com>
2023-03-02 19:32:34 +0800

    setup swagger with tsoa

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import swaggerUi from "swagger-ui-express";
 import routes from './routes';
 import morganMiddleware from './lib/morgan';
 
@@ -15,6 +16,19 @@ class App {
     middlewares() {
         this.server.use(express.json());
         this.server.use(morganMiddleware);
+        this.server.use(express.static("public"));
+        this.server.use(
+            "/docs",
+            swaggerUi.serve,
+            swaggerUi.setup(undefined, {
+                swaggerOptions: {
+                    url: "/swagger.json",
+                    info: {
+                        title: 'rawr'
+                    }
+                },
+            })
+        );
     }
 
     routes() {
--- src/controllers/ping.ts
+++ src/controllers/ping.ts
@@ -1,8 +1,15 @@
+import { Get, Route } from "tsoa";
+
 interface PingResponse {
     message: string;
 }
 
+@Route("ping")
 export default class PingController {
+    /**
+     * Pings the server
+     */
+    @Get("/")
     public async getMessage(): Promise<PingResponse> {
         return {
             message: "pong",
